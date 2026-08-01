1dd2c91ee64db4b1950f69d6752028555e8f3d40
GitHub Action <action@github.com>
2026-08-01 02:02:05 +0000

    Update .NET packages to latest versions
    - dotnet-runtime: Updated to 10.0.10
    - dotnet-sdk: Updated to 10.0.302
    
    This update was automatically generated by the GitHub Actions workflow.

--- packages/tkmm/dotnet-runtime/package.mk
+++ packages/tkmm/dotnet-runtime/package.mk
@@ -2,13 +2,13 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dotnet-runtime"
-PKG_VERSION="10.0.9"
+PKG_VERSION="10.0.10"
 PKG_LICENSE="MIT"
 PKG_SITE="https://dotnet.microsoft.com/"
 PKG_LONGDESC=".NET Runtime is a set of libraries to run .NET applications."
 PKG_TOOLCHAIN="manual"
 PKG_AUTORECONF="no"
-PKG_SHA256="7481dadb5d01fe9d3f309fb7e86a331a207217adc4f9684e6b009f58c3e303d2"
+PKG_SHA256="30e9dccd4c68454c2ffc7507d4d26bab01fe0a5291eb635beedf65d8d2f7e8ea"
 PKG_URL="https://builds.dotnet.microsoft.com/dotnet/Runtime/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}-linux-arm64.tar.gz"
 PKG_SOURCE_NAME="${PKG_NAME}-${PKG_VERSION}-${ARCH}.tar.gz"
 
--- packages/tkmm/dotnet-sdk/package.mk
+++ packages/tkmm/dotnet-sdk/package.mk
@@ -2,13 +2,13 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dotnet-sdk"
-PKG_VERSION="10.0.301"
+PKG_VERSION="10.0.302"
 PKG_LICENSE="MIT"
 PKG_SITE="https://dotnet.microsoft.com/"
 PKG_LONGDESC=".NET SDK is a set of libraries and tools that developers use to create .NET applications and libraries."
 PKG_TOOLCHAIN="manual"
 PKG_AUTORECONF="no"
-PKG_SHA256="4f4edaa4214636a0625de8f2351557b2024eae61beff0ac7c77f502d65681d28"
+PKG_SHA256="1c56318e4099990719f6369184e08bbad0248c09c5ad7532d2516e3cdfc3ab6d"
 PKG_URL="https://builds.dotnet.microsoft.com/dotnet/Sdk/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}-linux-arm64.tar.gz"
 PKG_SOURCE_NAME="${PKG_NAME}-${PKG_VERSION}-${ARCH}.tar.gz"
 
